7501cc5d81a7be42d77aaff6df6ea974e35862ed
Aryank35 <aryan5935kishan@gmail.com>
2023-10-19 21:52:11 +0530

    updated nav menu and image slider positions

---
+++ imageSlide.js
@@ -0,0 +1,11 @@
+const images = {
+    img1:"photos\grp-001.png",
+    img2:"photos\grp-002.png",
+    img3:"photos\grp-003.jpg",
+    img4:"photos\grp-004.jpg",
+    img5:"photos\grp-005.jpg",
+    img6:"photos\nature-001.jpg",
+    img7:"photos\nature-002.jpg",
+    img8:"photos\nature-003.jpg",
+    img9:"photos\nature-004.jpg"
+}
